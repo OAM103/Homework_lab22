a3eb1aa9e5b52b7c3e689ba764d78f42acdbaa95
OAM103 <113307610+OAM103@users.noreply.github.com>
2022-12-03 20:08:21 +0200

    Add files via upload

---
+++ RPN.java
@@ -0,0 +1,55 @@
+package com.metanit;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RPN{
+
+    public static void eval(String S) {
+        String[] str_s = S.split(" ");
+        Stack<Double> stack = new Stack<>();
+        for (String str : str_s) {
+            try {
+                if (str.equals("+")) {
+                    double val2 = stack.pop();
+                    double val1 = stack.pop();
+                    double result = val1 + val2;
+                    stack.push(result);
+
+                } else if (str.equals("-")) {
+                    double val2 = stack.pop();
+                    double val1 = stack.pop();
+                    double result = val1 - val2;
+                    stack.push(result);
+
+                } else if (str.equals("/")) {
+                    double val2 = stack.pop();
+                    double val1 = stack.pop();
+                    double result = val1 / val2;
+                    stack.push(result);
+
+                } else if (str.equals("*")) {
+                    double val2 = stack.pop();
+                    double val1 = stack.pop();
+                    double result = val1 * val2;
+                    stack.push(result);
+
+                } else {
+                    stack.push(Double.parseDouble(str));
+                }
+
+            }catch (IllegalArgumentException e){
+                continue;
+            }
+        }
+        double result = stack.pop();
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.nextLine();
+        eval(S);
+
+    }
+}
+
